52d3aff1a1fa4783d477d81206aca01642d45ca7
Matt Pahuta <mhpahuta@gmail.com>
2025-01-28 16:07:10 -0800

    Add mobile nav styles and JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('[aria-controls="primary-nav"]');
+const primaryNav = document.querySelector('#primary-nav');
+
+navToggle.addEventListener('click', () => {
+  const isNavOpen = navToggle.getAttribute('aria-expanded');
+
+  isNavOpen === 'false' 
+    ? navToggle.setAttribute('aria-expanded', 'true') 
+    : navToggle.setAttribute('aria-expanded', 'false')
+
+  console.log(isNavOpen)
+})
